6a083a226d46b756e524d93c3c6d0e34a1ecf301
Valeriy Levanov <levanov.vd@phystech.edu>
2022-09-20 11:49:51 +0300

    version2

--- C-spells/main.cpp
+++ C-spells/main.cpp
@@ -1,6 +1,26 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
+long long TryToFindMin(const int* a, int* opt, int i, int n) {
+  int kl = 0, kp = 0, sl = 1, sp = 1;
+  long long x1 = 0;
+  while (i + sp < n && a[i + sp] >= a[i]) {
+    ++kp;
+    ++sp;
+  }
+  while (i - sl > -1 && a[i - sl] >= a[i]) {
+    if (opt[i - sl] == 0) {
+      ++kl;
+      ++sl;
+    } else {
+      sl += opt[i - sl];
+      kl += opt[i - sl];
+    }
+  }
+  opt[i] = kl;
+  x1 = (kl + kp + 1) * a[i];
+  return x1;
+}
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
@@ -8,22 +28,19 @@ int main() {
   long long max1 = 0;
   cin >> n;
   int* a = new int[n];
+  int* optimization = new int[n];
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
   }
   for (int i = 0; i < n; ++i) {
-    long long kl = 0, kp = 0, sl = 1, sp = 1;
-    while (i - sl > -1 && a[i - sl] >= a[i]) {
-      ++kl;
-      ++sl;
-    }
-    while (i + sp < n && a[i + sp] >= a[i]) {
-      ++kp;
-      ++sp;
-    }
-    max1 = max((kl + kp + 1) * a[i], max1);
+    long long k = TryToFindMin(a, optimization, i, n);
+    max1 = max(max1, k);
+  }
+  for (int i = 0; i < n; ++i) {
+    cout << optimization[i] << " ";
   }
   cout << max1;
   delete[] a;
+  delete[] optimization;
   return 0;
 }
