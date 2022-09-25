a5a5441b995fec7f655fb51de365a7d5d4440220
Valeriy Levanov <levanov.vd@phystech.edu>
2022-09-25 22:41:16 +0300

    Super solution

--- GGoblins/main.cpp
+++ GGoblins/main.cpp
@@ -1,32 +1,34 @@
 #include <deque>
 #include <iostream>
 using namespace std;
-void Vhod1(deque<int>& first, deque<int>& second, int& count, char k, int i) {
-  if (k == '+' && count % 2 == 1) {
-    second.push_back(i);
+void DoEasyInput(deque<int>& first, deque<int>& second, int& count,
+                 char type_of_operation, int number_of_goblin) {
+  if (type_of_operation == '+' && count % 2 == 1) {
+    second.push_back(number_of_goblin);
   }
-  if (k == '+' && count % 2 == 0) {
-    second.push_back(i);
-    int r = second.front();
-    first.push_back(r);
+  if (type_of_operation == '+' && count % 2 == 0) {
+    second.push_back(number_of_goblin);
+    int number_of_change = second.front();
+    first.push_back(number_of_change);
     second.pop_front();
   }
-  if (k == '*' && count % 2 == 1) {
-    second.push_front(i);
+  if (type_of_operation == '*' && count % 2 == 1) {
+    second.push_front(number_of_goblin);
   }
-  if (k == '*' && count % 2 == 0) {
-    first.push_back(i);
+  if (type_of_operation == '*' && count % 2 == 0) {
+    first.push_back(number_of_goblin);
   }
 }
-void Vhod2(deque<int>& first, deque<int>& second, int& count, char k) {
-  if (k == '-' && count % 2 == 1) {
-    int r = first.front();
-    cout << r << endl;
+void DoHardInput(deque<int>& first, deque<int>& second, int& count,
+                 char type_of_operation) {
+  if (type_of_operation == '-' && count % 2 == 1) {
+    int number_of_outsider = first.front();
+    cout << number_of_outsider << endl;
     first.pop_front();
   }
-  if (k == '-' && count % 2 == 0) {
-    int r = first.front();
-    cout << r << endl;
+  if (type_of_operation == '-' && count % 2 == 0) {
+    int number_of_outsider = first.front();
+    cout << number_of_outsider << endl;
     first.pop_front();
     first.push_back(second.front());
     second.pop_front();
@@ -41,18 +43,18 @@ int main() {
   int count = 0;
   cin >> num;
   for (int i = 0; i < num; i++) {
-    char vvod1;
-    int vvod2 = 0;
-    cin >> vvod1;
-    if (vvod1 != '-') {
-      cin >> vvod2;
+    char type_of_operation;
+    int number = 0;
+    cin >> type_of_operation;
+    if (type_of_operation != '-') {
+      cin >> number;
     }
-    Vhod1(firsthalf, secondhalf, count, vvod1, vvod2);
-    Vhod2(firsthalf, secondhalf, count, vvod1);
-    if (vvod1 != '-') {
+    DoEasyInput(firsthalf, secondhalf, count, type_of_operation, number);
+    DoHardInput(firsthalf, secondhalf, count, type_of_operation);
+    if (type_of_operation != '-') {
       ++count;
     }
-    if (vvod1 == '-') {
+    if (type_of_operation == '-') {
       --count;
     }
   }
