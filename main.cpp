3baf5983f7db8004dc171a80396c77c4d0a788ef
MonosUna <113590557+MonosUna@users.noreply.github.com>
2022-10-19 20:33:02 +0300

    Second Commit
    https://contest.yandex.ru/contest/39305/run-report/72509234/  
    https://contest.yandex.ru/contest/39305/run-report/70618043/
    В первый раз создал структуру, надеюсь не сделал ничего запрещенного.

--- main.cpp
+++ main.cpp
@@ -1,62 +1,74 @@
 #include <deque>
 #include <iostream>
-using namespace std;
-void DoEasyInput(deque<int>& first, deque<int>& second, int& count,
-                 char type_of_operation, int number_of_goblin) {
-  if (type_of_operation == '+' && count % 2 == 1) {
-    second.push_back(number_of_goblin);
-  }
-  if (type_of_operation == '+' && count % 2 == 0) {
-    second.push_back(number_of_goblin);
-    int number_of_change = second.front();
-    first.push_back(number_of_change);
-    second.pop_front();
-  }
-  if (type_of_operation == '*' && count % 2 == 1) {
-    second.push_front(number_of_goblin);
-  }
-  if (type_of_operation == '*' && count % 2 == 0) {
-    first.push_back(number_of_goblin);
-  }
-}
-void DoHardInput(deque<int>& first, deque<int>& second, int& count,
-                 char type_of_operation) {
-  if (type_of_operation == '-' && count % 2 == 1) {
-    int number_of_outsider = first.front();
-    cout << number_of_outsider << endl;
-    first.pop_front();
-  }
-  if (type_of_operation == '-' && count % 2 == 0) {
-    int number_of_outsider = first.front();
-    cout << number_of_outsider << endl;
-    first.pop_front();
-    first.push_back(second.front());
-    second.pop_front();
-  }
-}
-int main() {
-  deque<int> firsthalf;
-  deque<int> secondhalf;
-  firsthalf.clear();
-  secondhalf.clear();
-  int num = 0;
+struct GoblinsQueue
+{
+  std::deque<int> firsthalf;
+  std::deque<int> secondhalf;
   int count = 0;
-  cin >> num;
+
+  void DoAdditionRequest(int number_of_goblin) {
+    if (count % 2 == 1) {
+      secondhalf.push_back(number_of_goblin);
+    } else {
+      secondhalf.push_back(number_of_goblin);
+      int number_of_change = secondhalf.front();
+      firsthalf.push_back(number_of_change);
+      secondhalf.pop_front();
+    }
+  }
+
+  void DoMultiplicationRequest(int number_of_goblin) {
+    if (count % 2 == 1) {
+      secondhalf.push_front(number_of_goblin);
+    }
+    if (count % 2 == 0) {
+      firsthalf.push_back(number_of_goblin);
+    }
+  }
+
+  void DoSubtractionRequest() {
+    if (count % 2 == 1) {
+      int number_of_outsider = firsthalf.front();
+      std::cout << number_of_outsider << '\n';
+      firsthalf.pop_front();
+    }
+    if (count % 2 == 0) {
+      int number_of_outsider = firsthalf.front();
+      std::cout << number_of_outsider << '\n';
+      firsthalf.pop_front();
+      firsthalf.push_back(secondhalf.front());
+      secondhalf.pop_front();
+    }
+  }
+
+  void DoInput(char type_of_input) {
+    if (type_of_input == '+') {
+      int number_of_goblin;
+      std::cin >> number_of_goblin;
+      DoAdditionRequest(number_of_goblin);
+      count++;
+    }
+    if (type_of_input == '*') {
+      int number_of_goblin;
+      std::cin >> number_of_goblin;
+      DoMultiplicationRequest(number_of_goblin);
+      count++;
+    }
+    if (type_of_input == '-') {
+      DoSubtractionRequest();
+      count--;
+    }
+  }
+};
+
+int main() {
+  int num = 0;
+  std::cin >> num;
+  GoblinsQueue queue;
   for (int i = 0; i < num; i++) {
-    char type_of_operation;
-    int number = 0;
-    cin >> type_of_operation;
-    if (type_of_operation != '-') {
-      cin >> number;
-    }
-    DoEasyInput(firsthalf, secondhalf, count, type_of_operation, number);
-    DoHardInput(firsthalf, secondhalf, count, type_of_operation);
-    if (type_of_operation != '-') {
-      ++count;
-    }
-    if (type_of_operation == '-') {
-      --count;
-    }
+    char type_of_input;
+    std::cin >> type_of_input;
+    queue.DoInput(type_of_input);
   }
   return 0;
 }
