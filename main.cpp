c089afaeeacde7ccb3199894bc1eaeb26677ad6c
MonosUna <113590557+MonosUna@users.noreply.github.com>
2022-10-20 20:45:10 +0300

    Try to optimize struct
    https://contest.yandex.ru/contest/39305/run-report/72602886/ сама задача
    https://contest.yandex.ru/contest/39305/run-report/70848527/ 1 посылка

--- main.cpp
+++ main.cpp
@@ -5,61 +5,65 @@ struct GoblinsQueue
   std::deque<int> firsthalf;
   std::deque<int> secondhalf;
   int count = 0;
-
-  void DoAdditionRequest(int number_of_goblin) {
-    if (count % 2 == 1) {
-      secondhalf.push_back(number_of_goblin);
-    } else {
-      secondhalf.push_back(number_of_goblin);
-      int number_of_change = secondhalf.front();
-      firsthalf.push_back(number_of_change);
-      secondhalf.pop_front();
-    }
-  }
-
-  void DoMultiplicationRequest(int number_of_goblin) {
-    if (count % 2 == 1) {
-      secondhalf.push_front(number_of_goblin);
-    }
-    if (count % 2 == 0) {
-      firsthalf.push_back(number_of_goblin);
-    }
-  }
-
-  void DoSubtractionRequest() {
-    if (count % 2 == 1) {
-      int number_of_outsider = firsthalf.front();
-      std::cout << number_of_outsider << '\n';
-      firsthalf.pop_front();
-    }
-    if (count % 2 == 0) {
-      int number_of_outsider = firsthalf.front();
-      std::cout << number_of_outsider << '\n';
-      firsthalf.pop_front();
-      firsthalf.push_back(secondhalf.front());
-      secondhalf.pop_front();
-    }
-  }
-
-  void DoInput(char type_of_input) {
-    if (type_of_input == '+') {
-      int number_of_goblin;
-      std::cin >> number_of_goblin;
-      DoAdditionRequest(number_of_goblin);
-      count++;
-    }
-    if (type_of_input == '*') {
-      int number_of_goblin;
-      std::cin >> number_of_goblin;
-      DoMultiplicationRequest(number_of_goblin);
-      count++;
-    }
-    if (type_of_input == '-') {
-      DoSubtractionRequest();
-      count--;
-    }
-  }
+  void DoAdditionRequest(int number_of_goblin);
+  void DoMultiplicationRequest(int number_of_goblin);
+  void DoSubtractionRequest();
+  void DoInput(char type_of_input);
 };
+
+void GoblinsQueue::DoAdditionRequest(int number_of_goblin) {
+  if (count % 2 == 1) {
+    secondhalf.push_back(number_of_goblin);
+  } else {
+    secondhalf.push_back(number_of_goblin);
+    int number_of_change = secondhalf.front();
+    firsthalf.push_back(number_of_change);
+    secondhalf.pop_front();
+  }
+}
+
+void GoblinsQueue::DoMultiplicationRequest(int number_of_goblin) {
+  if (count % 2 == 1) {
+    secondhalf.push_front(number_of_goblin);
+  }
+  if (count % 2 == 0) {
+    firsthalf.push_back(number_of_goblin);
+  }
+}
+
+void GoblinsQueue::DoSubtractionRequest() {
+  if (count % 2 == 1) {
+    int number_of_outsider = firsthalf.front();
+    std::cout << number_of_outsider << '\n';
+    firsthalf.pop_front();
+  }
+  if (count % 2 == 0) {
+    int number_of_outsider = firsthalf.front();
+    std::cout << number_of_outsider << '\n';
+    firsthalf.pop_front();
+    firsthalf.push_back(secondhalf.front());
+    secondhalf.pop_front();
+  }
+}
+
+void GoblinsQueue::DoInput(char type_of_input) {
+  if (type_of_input == '+') {
+    int number_of_goblin;
+    std::cin >> number_of_goblin;
+    DoAdditionRequest(number_of_goblin);
+    count++;
+  }
+  if (type_of_input == '*') {
+    int number_of_goblin;
+    std::cin >> number_of_goblin;
+    DoMultiplicationRequest(number_of_goblin);
+    count++;
+  }
+  if (type_of_input == '-') {
+    DoSubtractionRequest();
+    count--;
+  }
+}
 
 int main() {
   int num = 0;
