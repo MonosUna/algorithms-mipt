b21053004ef627bfa4ff6188efc25d94d75febbb
MonosUna <113590557+MonosUna@users.noreply.github.com>
2022-10-25 09:22:31 +0300

    Correct input, Add task condition
    https://contest.yandex.ru/contest/39305/run-report/70848527/ первая посылка
    https://contest.yandex.ru/contest/39305/run-report/72966698/ текущая посылка

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
+//В первой строке входных данный записано число
+// N (1 ≤ N ≤ 105) — количество запросов к программе.
+// Следующие N строк содержат описание запросов в формате:
+//«+ i» — гоблин с номером i (1 ≤ i ≤ N) встает в конец очереди;
+//«* i» — привилегированный гоблин с номером i встает в середину очереди;
+//«-» — первый гоблин из очереди уходит к шаманам.
+// Гарантируется, что на момент такого запроса очередь не пуста.
+//Для каждого запроса типа «-» программа должна вывести номер гоблина,
+// который должен зайти к шаманам.
 #include <deque>
 #include <iostream>
-struct GoblinsQueue
-{
+struct GoblinsQueue {
   std::deque<int> firsthalf;
   std::deque<int> secondhalf;
   int count = 0;
-  void DoAdditionRequest(int number_of_goblin);
-  void DoMultiplicationRequest(int number_of_goblin);
+  void AddInTheUsualWay(int number_of_goblin);
+  void AddWithPriority(int number_of_goblin);
   void DoSubtractionRequest();
-  void DoInput(char type_of_input);
 };
 
-void GoblinsQueue::DoAdditionRequest(int number_of_goblin) {
+void GoblinsQueue::AddInTheUsualWay(int number_of_goblin) {
   if (count % 2 == 1) {
     secondhalf.push_back(number_of_goblin);
   } else {
@@ -22,7 +29,7 @@ void GoblinsQueue::DoAdditionRequest(int number_of_goblin) {
   }
 }
 
-void GoblinsQueue::DoMultiplicationRequest(int number_of_goblin) {
+void GoblinsQueue::AddWithPriority(int number_of_goblin) {
   if (count % 2 == 1) {
     secondhalf.push_front(number_of_goblin);
   }
@@ -46,25 +53,6 @@ void GoblinsQueue::DoSubtractionRequest() {
   }
 }
 
-void GoblinsQueue::DoInput(char type_of_input) {
-  if (type_of_input == '+') {
-    int number_of_goblin;
-    std::cin >> number_of_goblin;
-    DoAdditionRequest(number_of_goblin);
-    count++;
-  }
-  if (type_of_input == '*') {
-    int number_of_goblin;
-    std::cin >> number_of_goblin;
-    DoMultiplicationRequest(number_of_goblin);
-    count++;
-  }
-  if (type_of_input == '-') {
-    DoSubtractionRequest();
-    count--;
-  }
-}
-
 int main() {
   int num = 0;
   std::cin >> num;
@@ -72,7 +60,22 @@ int main() {
   for (int i = 0; i < num; i++) {
     char type_of_input;
     std::cin >> type_of_input;
-    queue.DoInput(type_of_input);
+    if (type_of_input == '+') {
+      int number_of_goblin;
+      std::cin >> number_of_goblin;
+      queue.AddInTheUsualWay(number_of_goblin);
+      queue.count++;
+    }
+    if (type_of_input == '*') {
+      int number_of_goblin;
+      std::cin >> number_of_goblin;
+      queue.AddWithPriority(number_of_goblin);
+      queue.count++;
+    }
+    if (type_of_input == '-') {
+      queue.DoSubtractionRequest();
+      queue.count--;
+    }
   }
   return 0;
 }
