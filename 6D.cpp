c9e0548b73b370d127238bd9c5bc39538cd964d7
monosuna <levanov.vd@phystech.edu>
2023-02-26 21:34:53 +0300

    create function

--- 6D.cpp
+++ 6D.cpp
@@ -2,13 +2,7 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  int floor, count_of_planes;
-  std::cin >> floor >> count_of_planes;
-  int log2 = static_cast<int>(std::log2(floor));
-  if (count_of_planes > log2) {
-    count_of_planes = 1 + log2;
-  }
+int FindAnswer(int floor, int count_of_planes) {
   std::vector<int> previous_dp(floor + 1, 0);
   for (int i = 2; i <= floor; ++i) {
     previous_dp[i] = i - 1;
@@ -21,22 +15,31 @@ int main() {
     int optimal_index = 1;
     for (int j = 3; j <= floor; ++j) {
       while (optimal_index <= j &&
-          previous_dp[optimal_index] < last_dp[j - optimal_index]) {
+             previous_dp[optimal_index] < last_dp[j - optimal_index]) {
         ++optimal_index;
       }
       last_dp[j] = std::min(std::max(previous_dp[optimal_index],
                                      last_dp[j - optimal_index]),
                             std::max(previous_dp[optimal_index + 1],
                                      last_dp[j - optimal_index - 1])) +
-          1;
+                   1;
     }
     previous_dp = last_dp;
     answer = last_dp[floor];
   }
   if (count_of_planes == 0 && floor > 1) {
-    std::cout << -1;
-  } else {
-    std::cout << answer;
+    answer = -1;
   }
+  return answer;
+}
+
+int main() {
+  int floor, count_of_planes;
+  std::cin >> floor >> count_of_planes;
+  int optimal_count = static_cast<int>(std::log2(floor)) + 1;
+  if (count_of_planes > optimal_count) {
+    count_of_planes = optimal_count;
+  }
+  std::cout << FindAnswer(floor, count_of_planes);
   return 0;
 }
