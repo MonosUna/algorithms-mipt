7597a42bfb5e25be9afd39df3e61de24ad6c25a3
monosuna <levanov.vd@phystech.edu>
2023-04-10 19:47:31 +0300

    Modify function

--- 6B.cpp
+++ 6B.cpp
@@ -2,31 +2,31 @@
 #include <stack>
 #include <vector>
 
-int FindPos(std::vector<std::pair<int, int>>& array, int number, int index) {
-  int last = static_cast<int>(array.size() - 1);
-  if (array.back().first >= number) {
-    array.emplace_back(number, index);
+int FindPos(std::vector<std::pair<int, int>>& dp, int number, int index) {
+  int last = static_cast<int>(dp.size() - 1);
+  if (dp.back().first >= number) {
+    dp.emplace_back(number, index);
     return last + 1;
   }
   int l = 1, r = last;
   while (r - l > 1) {
     int m = (r + l) / 2;
-    if (array[m].first >= number) {
+    if (dp[m].first >= number) {
       l = m;
     } else {
       r = m;
     }
   }
-  if (array[l].first < number) {
-    array[l] = std::make_pair(number, index);
+  if (dp[l].first < number) {
+    dp[l] = std::make_pair(number, index);
     return l;
   }
-  array[r] = std::make_pair(number, index);
+  dp[r] = std::make_pair(number, index);
   return r;
 }
 
-int FindMaxLength(int count, std::vector<std::pair<int, int>>& dp,
-                  std::vector<int>& positions, std::vector<int>& numbers) {
+void CalculateDP(int count, std::vector<std::pair<int, int>>& dp,
+                 std::vector<int>& positions, std::vector<int>& numbers) {
   int max_length = 1;
   for (int i = 2; i <= count; ++i) {
     int pos = FindPos(dp, numbers[i], i);
@@ -35,7 +35,6 @@ int FindMaxLength(int count, std::vector<std::pair<int, int>>& dp,
       ++max_length;
     }
   }
-  return max_length;
 }
 
 std::stack<int> FindSequence(std::vector<std::pair<int, int>>& dp,
@@ -67,7 +66,8 @@ int main() {
   dp[1].first = numbers[1];
   dp[1].second = 1;
   std::vector<int> positions(count + 1);
-  int max_length = FindMaxLength(count, dp, positions, numbers);
+  CalculateDP(count, dp, positions, numbers);
+  int max_length = static_cast<int>(dp.size()) - 1;
   std::cout << max_length << "\n";
   PrintSequence(FindSequence(dp, max_length, positions), max_length);
   return 0;
