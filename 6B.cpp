1bb55699bd1a397a49e1b79013970ed60815a2de
monosuna <levanov.vd@phystech.edu>
2023-04-11 11:04:54 +0300

    Do function decomposition

--- 6B.cpp
+++ 6B.cpp
@@ -27,18 +27,14 @@ int FindPos(std::vector<std::pair<int, int>>& dp, int number, int index) {
 
 void CalculateDP(int count, std::vector<std::pair<int, int>>& dp,
                  std::vector<int>& positions, std::vector<int>& numbers) {
-  int max_length = 1;
   for (int i = 2; i <= count; ++i) {
     int pos = FindPos(dp, numbers[i], i);
     positions[i] = dp[pos - 1].second;
-    if (pos > max_length) {
-      ++max_length;
-    }
   }
 }
 
-std::stack<int> FindSequence(std::vector<std::pair<int, int>>& dp,
-                             int max_length, std::vector<int>& positions) {
+std::stack<int> CreateSequenceIndexes(std::vector<std::pair<int, int>>& dp,
+                                      int max_length, std::vector<int>& positions) {
   std::stack<int> order;
   int last_index = dp[max_length].second;
   while (last_index != 0) {
@@ -48,27 +44,33 @@ std::stack<int> FindSequence(std::vector<std::pair<int, int>>& dp,
   return order;
 }
 
-void PrintSequence(std::stack<int> sequence, int max_length) {
-  for (int i = 1; i <= max_length; ++i) {
+void PrintStack(std::stack<int> sequence) {
+  while(!sequence.empty()) {
     std::cout << sequence.top() << " ";
     sequence.pop();
   }
 }
 
-int main() {
-  int count;
-  std::cin >> count;
+std::stack<int> FindSequenceIndices(std::vector<int>& numbers, int count) {
   std::vector<std::pair<int, int>> dp(2);
-  std::vector<int> numbers(count + 1);
-  for (int i = 1; i <= count; ++i) {
-    std::cin >> numbers[i];
-  }
+//  дп хранит в ячейке i - наибольший возможный элемент последний элемент НнВП
+//  длины i
   dp[1].first = numbers[1];
   dp[1].second = 1;
   std::vector<int> positions(count + 1);
   CalculateDP(count, dp, positions, numbers);
   int max_length = static_cast<int>(dp.size()) - 1;
   std::cout << max_length << "\n";
-  PrintSequence(FindSequence(dp, max_length, positions), max_length);
+  return CreateSequenceIndexes(dp, max_length, positions);
+}
+
+int main() {
+  int count;
+  std::cin >> count;
+  std::vector<int> numbers(count + 1);
+  for (int i = 1; i <= count; ++i) {
+    std::cin >> numbers[i];
+  }
+  PrintStack(FindSequenceIndices(numbers, count));
   return 0;
 }
