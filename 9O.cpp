93dacb65ae3ff41d0a38a8419ef19b83b62b23c4
monosuna <levanov.vd@phystech.edu>
2023-05-15 16:54:45 +0300

    add functional decomposition

--- 9O.cpp
+++ 9O.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <vector>
-#include <queue>
 #include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
 
 struct Edge {
   int from;
@@ -9,27 +9,24 @@ struct Edge {
   double cap;
   double flow;
 
-  Edge(int from, int to, double cap, double flow) : from(from), to(to), cap(cap), flow(flow) {}
+  Edge(int from, int to, double cap, double flow)
+      : from(from), to(to), cap(cap), flow(flow) {}
   Edge() : from(0), to(0), cap(0.0), flow(0.0) {}
 
   double get_left_cap() const { return cap - flow; }
-
 };
 
-int num_of_other(int k) {
-  return (k % 2 == 0) ? (k + 1) : (k - 1);
-}
+int num_of_other(int k) { return (k % 2 == 0) ? (k + 1) : (k - 1); }
 
 struct VertexInfo {
   std::vector<int> edges_nums;
   int ptr;
   int group;
 
-  VertexInfo(): edges_nums(), ptr(), group(-1) { }
-
+  VertexInfo() : edges_nums(), ptr(), group(-1) {}
 };
 
-void bfs(std::vector<VertexInfo>& vertices, std::vector<Edge>& edges) {
+void bfs(std::vector<VertexInfo>& vertices, const std::vector<Edge>& edges) {
   for (int i = 1; i < vertices.size(); ++i) {
     vertices[i].group = -1;
   }
@@ -49,7 +46,8 @@ void bfs(std::vector<VertexInfo>& vertices, std::vector<Edge>& edges) {
   }
 }
 
-void dfs(int vertex, std::vector<bool>& used, std::vector<Edge>& edges, std::vector<VertexInfo>& vertices) {
+void dfs(int vertex, std::vector<bool>& used, const std::vector<Edge>& edges,
+         const std::vector<VertexInfo>& vertices) {
   used[vertex] = true;
   for (int num : vertices[vertex].edges_nums) {
     int to = edges[num].to;
@@ -59,18 +57,25 @@ void dfs(int vertex, std::vector<bool>& used, std::vector<Edge>& edges, std::vec
   }
 }
 
-double dfsDinica(int vertex, double current_flow, std::vector<VertexInfo>& vertices,
-              std::vector<Edge>& edges) {
+double dfsAddingFlow(int vertex, double current_flow,
+                     std::vector<VertexInfo>& vertices,
+                     std::vector<Edge>& edges) {
   int last = static_cast<int>(vertices.size()) - 1;
-  if (vertex == last) { return current_flow; }
-  while (vertices[vertex].ptr < static_cast<int>(vertices[vertex].edges_nums.size())) {
+  if (vertex == last) {
+    return current_flow;
+  }
+  while (vertices[vertex].ptr <
+      static_cast<int>(vertices[vertex].edges_nums.size())) {
     int e_num = vertices[vertex].edges_nums[vertices[vertex].ptr];
     int to = edges[e_num].to;
-    if (vertices[vertex].group + 1 != vertices[to].group || std::abs(edges[e_num].get_left_cap()) < 0.00001 ) {
+    if (vertices[vertex].group + 1 != vertices[to].group ||
+        std::abs(edges[e_num].get_left_cap()) < 0.00001) {
       vertices[vertex].ptr = vertices[vertex].ptr + 1;
       continue;
     }
-    double x = dfsDinica(to, std::min(current_flow, edges[e_num].get_left_cap()), vertices, edges);
+    double x =
+        dfsAddingFlow(to, std::min(current_flow, edges[e_num].get_left_cap()),
+                      vertices, edges);
     if (std::abs(x) < 0.00001) {
       vertices[vertex].ptr = vertices[vertex].ptr + 1;
       continue;
@@ -84,8 +89,8 @@ double dfsDinica(int vertex, double current_flow, std::vector<VertexInfo>& verti
 
 class network {
  public:
-  network(double g, std::vector<std::pair<int, int>>& other_edges, int count_of_vertices,
-          std::vector<int>& degrees) {
+  network(double g, const std::vector<std::pair<int, int>>& other_edges,
+          int count_of_vertices, const std::vector<int>& degrees) {
     finish = count_of_vertices + 1;
     count_of_vertices_ = finish + 1;
     std::vector<VertexInfo> vertices1(count_of_vertices_);
@@ -107,7 +112,9 @@ class network {
       vertices1[0].edges_nums.push_back(e);
       vertices1[i].edges_nums.push_back(e + 1);
       e += 2;
-      edges.emplace_back(i, finish, edges_capacity + 2 * g - static_cast<double>(degrees[i]), 0.0);
+      edges.emplace_back(
+          i, finish, edges_capacity + 2 * g - static_cast<double>(degrees[i]),
+          0.0);
       edges.emplace_back(finish, i, 0.0, 0.0);
       vertices1[i].edges_nums.push_back(e);
       vertices1[finish].edges_nums.push_back(e + 1);
@@ -116,17 +123,19 @@ class network {
     vertices = vertices1;
   }
 
-  std::vector<int> fin_min_cut() {
+  std::vector<int> FindMinCut() {
     double max_start_flow = 100000.0;
     double max_flow = 0;
     while (true) {
       bfs(vertices, edges);
-      if (vertices[finish].group == -1) { break; }
+      if (vertices[finish].group == -1) {
+        break;
+      }
       for (int i = 0; i <= finish; ++i) {
         vertices[i].ptr = 0;
       }
       while (true) {
-        double x = dfsDinica(0, max_start_flow, vertices, edges);
+        double x = dfsAddingFlow(0, max_start_flow, vertices, edges);
         max_flow += x;
         if (std::abs(x) < 0.00001) {
           break;
@@ -153,9 +162,31 @@ class network {
   int finish;
   std::vector<Edge> edges;
   std::vector<VertexInfo> vertices;
-
 };
 
+std::vector<int> FindMaxDensitySubgraph(
+    int count_of_vertices, int count_of_edges,
+    const std::vector<std::pair<int, int>>& edges,
+    const std::vector<int>& degrees) {
+  double left = 0.0;
+  auto right = static_cast<double>(count_of_edges);
+  auto minimal_distance =
+      1.0 / static_cast<double>(count_of_vertices * count_of_vertices -
+          count_of_vertices);
+  std::vector<int> vertices_in_subgraph;
+  while (right - left >= minimal_distance) {
+    double medium = (right + left) / 2;
+    network net(medium, edges, count_of_vertices, degrees);
+    auto vertices_in_min_cut = net.FindMinCut();
+    if (vertices_in_min_cut.size() == 1) {
+      right = medium;
+    } else {
+      vertices_in_subgraph = vertices_in_min_cut;
+      left = medium;
+    }
+  }
+  return vertices_in_subgraph;
+}
 
 int main() {
   int count_of_vertices, count_of_edges;
@@ -169,21 +200,8 @@ int main() {
     degrees[from] += 1;
     degrees[to] += 1;
   }
-  double left = 0.0;
-  auto right = static_cast<double>(count_of_edges);
-  auto minimal_distance = 1.0 / static_cast<double>(count_of_vertices * count_of_vertices - count_of_vertices);
-  std::vector<int> vertices_in_subgraph;
-  while (right - left >= minimal_distance) {
-    double medium = (right + left) / 2;
-    network net(medium, edges, count_of_vertices, degrees);
-    auto vertices_in_min_cut = net.fin_min_cut();
-    if (vertices_in_min_cut.size() == 1) {
-      right = medium;
-    } else {
-      vertices_in_subgraph = vertices_in_min_cut;
-      left = medium;
-    }
-  }
+  auto vertices_in_subgraph =
+      FindMaxDensitySubgraph(count_of_vertices, count_of_edges, edges, degrees);
   if (count_of_edges == 0) {
     std::cout << 1 << "\n" << 1;
     return 0;
