60abd46284a15f2cc517b48b5a1b6919365df4b9
monosuna <levanov.vd@phystech.edu>
2023-05-16 21:14:18 +0300

    rework

--- 9B.cpp
+++ 9B.cpp
@@ -20,47 +20,51 @@ class DSU {
     return sets[vertex] = find(sets[vertex]);
   }
 
-  void union_(int vertex1, int vertex2) {
-    int parent1 = find(vertex1);
-    int parent2 = find(vertex2);
-    if (dimensions[parent1] >= dimensions[parent2]) {
-      sets[parent2] = parent1;
-      dimensions[parent1] += parent2;
-    } else {
-      sets[parent1] = parent2;
-      dimensions[parent2] += parent1;
-    }
-  }
+  void union_(int vertex1, int vertex2);
 
  private:
   std::vector<int> sets;
   std::vector<int> dimensions;
 };
 
+void DSU::union_(int vertex1, int vertex2) {
+  int parent1 = find(vertex1);
+  int parent2 = find(vertex2);
+  if (dimensions[parent1] >= dimensions[parent2]) {
+    sets[parent2] = parent1;
+    dimensions[parent1] += parent2;
+  } else {
+    sets[parent1] = parent2;
+    dimensions[parent2] += parent1;
+  }
+}
+
 class Graph {
  public:
   explicit Graph(int count_of_vertices,
                  std::vector<std::pair<int, std::pair<int, int>>>& roads)
       : count_of_vertices(count_of_vertices), edges(roads) {}
-  int calculateMST() const {
-    int sum = 0;
-    DSU set(count_of_vertices);
-    for (std::pair<int, std::pair<int, int>> edge : edges) {
-      int first = edge.second.first;
-      int second = edge.second.second;
-      int weight = edge.first;
-      if (set.find(first) != set.find(second)) {
-        set.union_(first, second);
-        sum += weight;
-      }
-    }
-    return sum;
-  }
+  int calculateMST() const;
 
  private:
   int count_of_vertices;
   std::vector<std::pair<int, std::pair<int, int>>> edges;
 };
+
+int Graph::calculateMST() const {
+  int sum = 0;
+  DSU set(count_of_vertices);
+  for (std::pair<int, std::pair<int, int>> edge : edges) {
+    int first = edge.second.first;
+    int second = edge.second.second;
+    int weight = edge.first;
+    if (set.find(first) != set.find(second)) {
+      set.union_(first, second);
+      sum += weight;
+    }
+  }
+  return sum;
+}
 
 int main() {
   int count_of_vertices;
